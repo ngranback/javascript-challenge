868ab3f08d6dbfa1a69b8499d389448f9755f843
ngranback <nella.granback@gmail.com>
2021-07-25 22:05:01 -0500

    lvl2 - added shape filter

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -20,12 +20,24 @@ function runEnter() {
     // Prevent page from refreshing
     d3.event.preventDefault();
     // Get input value
-    var inputElement = d3.select("#datetime");
-    var inputValue = inputElement.property("value");
-    console.log(inputValue);
+    var inputDate = d3.select("#datetime").property("value");
+    console.log(inputDate);
 
-    var filteredData = data.filter(data => data.datetime === inputValue);
-    console.log(filteredData)
+    var inputShape = d3.select("#shape").property("value");
+    console.log(inputShape);
+
+    var filteredData = data;
+    
+    
+    if (inputDate != "") {
+      filteredData = filteredData.filter(data => data.datetime === inputDate);
+      console.log(filteredData)
+    };
+    if (inputShape != "Choose a shape...") { //only filter shape if a shape is chosen
+      filteredData = filteredData.filter(data => data.shape === inputShape);
+      console.log(filteredData)
+    };
+
 
     
       var tbody = d3.select("tbody");
